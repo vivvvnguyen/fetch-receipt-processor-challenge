e9cd46e7e76eff4fe982e0b0f36fc1bf07f64f11
Vivian Nguyen <viviannguyen@Vivians-MBP-2.lan>
2023-09-20 21:16:28 -0700

    port edit

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const port = 8000;
+const port = process.env.port || 8080;
 const app = express();
 app.use(express.json(), express.urlencoded({extended:true}), cors());
 
